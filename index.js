687de68a00ec184b407e82cc1453807be56e157a
clairemuiru1 <muiruclaire@gmail.com>
2023-10-15 14:00:08 +0300

    "Completed assignment"

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+// Define the writeCards function
+function writeCards(names, event) {
+    const thanks = [];
+    for (const name of names) {
+      thanks.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+    }
+    return thanks;
+  }
+  // Define the countDown function
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  // Example usage:
+  const names = ["Alice", "Bob", "Charlie"];
+  const event = "birthday";
+  const countdownNumber = 5;
+
+  console.log(writeCards(names, event));
+  countDown(countdownNumber);
